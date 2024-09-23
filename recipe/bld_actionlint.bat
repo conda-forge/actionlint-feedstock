1ca5ba7d3d92cc49abf97ce1c9aadee1b8666117
Nicholas Bollweg <nick.bollweg@gmail.com>
2024-09-23 16:53:41 -0500

    try sibling outputs

--- recipe/bld_actionlint.bat
+++ recipe/bld_actionlint.bat
@@ -9,7 +9,7 @@ set MSYSTEM=MINGW%ARCH%
 set MSYS2_PATH_TYPE=inherit
 set CHERE_INVOKING=1
 
-bash -lc "./build_win.sh"
+bash -lc "./build_actionlint_win.sh"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 exit /b 0
