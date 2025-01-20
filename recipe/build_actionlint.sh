6f26859f82a7db268b170cc1e34eeace80ce1b1e
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-01-20 07:27:10 -0600

    MNT: Re-rendered with conda-build 24.11.2, conda-smithy 3.45.3, and conda-forge-pinning 2025.01.20.12.54.40

--- recipe/build_actionlint.sh
+++ recipe/build_actionlint.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
-
-set -eux
+set -eux -o pipefile
 
 module="github.com/rhysd/actionlint"
 
-export GOPATH="$( pwd )"
+GOPATH="$( pwd )"
+export GOPATH
 export GOROOT="${BUILD_PREFIX}/go"
 export GO_EXTLINK_ENABLED=1
 export CGO_ENABLED=1
 export GO111MODULE=on
 
-which go
+command -v go
 env | grep GOROOT
 go version
 
 pushd "src/${module}"
     go build \
         -buildmode=pie \
-        -ldflags "-s -w -X $module.version=${PKG_VERSION}" \
+        -ldflags "-s -w -X ${module}.version=${PKG_VERSION}" \
         -o "${PREFIX}/bin/${PKG_NAME}" \
         "./cmd/${PKG_NAME}" \
         || exit 1
@@ -29,4 +29,5 @@ popd
 ls "${PREFIX}/bin/${PKG_NAME}"
 
 # Make GOPATH directories writeable so conda-build can clean everything up.
-find "$( go env GOPATH )" -type d -exec chmod +w {} \;
+CLEAN_PATH="$( go env GOPATH )"
+find "${CLEAN_PATH}" -type d -exec chmod +w {} \;
